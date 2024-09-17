cf04d798694b64bd421bb79c9237a2b4043c4951
wstodgell <curtisstodgell2@gmail.com>
2024-09-17 10:55:32 -0700

    testgin and building test IOT

--- CDK/lib/ecr-stack.ts
+++ CDK/lib/ecr-stack.ts
@@ -5,24 +5,40 @@ import * as iam from 'aws-cdk-lib/aws-iam';
 
 export class EcrStack extends cdk.Stack {
   public readonly GPSEcrRepositoryUri: string;
+  public readonly TestEcrRepositoryUri: string;
 
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const GPSrepository = new ecr.Repository(this, 'MyIotGpsAppRepository', {
+    const GPSRepository = new ecr.Repository(this, 'MyIotGpsAppRepository', {
       repositoryName: 'my-iot-gps-app',
       emptyOnDelete: true,
       removalPolicy: cdk.RemovalPolicy.DESTROY,
     });
 
+    const TestRepository = new ecr.Repository(this, 'MyIotTestAppRepository', {
+      repositoryName: 'my-iot-test-app',
+      emptyOnDelete: true,
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+    });
+
     // store the repository - property of cdk.Stack URI is typically: aws_account_id.dkr.ecr.region.amazonaws.com/repository_name
-    this.GPSEcrRepositoryUri = GPSrepository.repositoryUri;
+    this.GPSEcrRepositoryUri = GPSRepository.repositoryUri;
+
+    // store the repository - property of cdk.Stack URI is typically: aws_account_id.dkr.ecr.region.amazonaws.com/repository_name
+    this.TestEcrRepositoryUri = TestRepository.repositoryUri;
 
     // Creates a Cloudformation output to display values: Logical ID = GPS_EcrRepositoryUri and name of the value.
     new cdk.CfnOutput(this, 'GPSEcrRepositoryUri', {
       value: this.GPSEcrRepositoryUri,
-      description: 'URI of the ECR repository',
+      description: 'URI of the GPS ECR repository',
       exportName: 'GPSEcrRepositoryUri'
+    });
+
+    new cdk.CfnOutput(this, 'TestEcrRepositoryUri', {
+      value: this.TestEcrRepositoryUri,
+      description: 'URI of the Test ECR repository',
+      exportName: 'TestEcrRepositoryUri'
     });
 
     // Create the IAM role with AdministratorAccess
