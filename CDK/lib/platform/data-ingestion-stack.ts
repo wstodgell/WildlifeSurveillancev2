3a48c28b3293ce906284e097deedeec5c12c9482
wstodgell <curtisstodgell2@gmail.com>
2024-09-25 13:47:08 -0700

    changed the folder structure of my project

---
+++ CDK/lib/platform/data-ingestion-stack.ts
@@ -0,0 +1,13 @@
+import * as cdk from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+import * as ecr from 'aws-cdk-lib/aws-ecr';
+import * as iam from 'aws-cdk-lib/aws-iam';
+
+export class DataIngestionStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+   
+
+  }
+}
