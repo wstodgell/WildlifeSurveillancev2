3031857a7896de83d61771fe6614da2749aa8af9
wstodgell <curtisstodgell2@gmail.com>
2024-09-25 13:50:36 -0700

    added s3 bucket for testing

--- CDK/lib/platform/data-ingestion-stack.ts
+++ CDK/lib/platform/data-ingestion-stack.ts
@@ -1,13 +1,22 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-import * as ecr from 'aws-cdk-lib/aws-ecr';
-import * as iam from 'aws-cdk-lib/aws-iam';
+import * as s3 from 'aws-cdk-lib/aws-s3';  // Import S3 module
 
 export class DataIngestionStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-   
+    // Create the most basic S3 bucket
+    const bucket = new s3.Bucket(this, 'BasicS3Bucket', {
+      // Default properties for a basic S3 bucket
+      removalPolicy: cdk.RemovalPolicy.DESTROY, // Ensures the bucket is deleted when the stack is destroyed
+      autoDeleteObjects: true,  // Automatically delete objects when the bucket is deleted
+    });
 
+    // Output the bucket name to the console
+    new cdk.CfnOutput(this, 'BucketNameOutput', {
+      value: bucket.bucketName,
+      description: 'The name of the S3 bucket',
+    });
   }
 }
