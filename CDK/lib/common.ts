6cbcc88ecbd3a7149ef2f9182d097f713f6f64a2
wstodgell <curtisstodgell2@gmail.com>
2024-10-01 06:23:10 -0700

    chagned the name of GPS_Transmitter.py

---
+++ CDK/lib/common.ts
@@ -0,0 +1,11 @@
+export var IOT_TOPIC_GPS = ''
+
+export var ECR_REPO_GPS = 'my-iot-gps-app'
+export var ECR_REPO_TEST = 'my-iot-test-app'
+
+
+export function createIoTThing(
+    thingName: string,
+    policyName: string,
+    region: string
+  ) {}
