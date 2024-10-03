b57e57936ab39734b165668705e5378d2f207774
wstodgell <curtisstodgell2@gmail.com>
2024-10-03 10:00:56 -0700

    added glue crawler and S3 bucket for dynamoDB data

--- CDK/lib/platform/data-ingestion-stack.ts
+++ CDK/lib/platform/data-ingestion-stack.ts
@@ -5,12 +5,30 @@ import * as lambda from 'aws-cdk-lib/aws-lambda';
 import * as iot from 'aws-cdk-lib/aws-iot';
 import * as iam from 'aws-cdk-lib/aws-iam';
 import * as dynamodb from 'aws-cdk-lib/aws-dynamodb';
+import { CfnCrawler, CfnDatabase } from 'aws-cdk-lib/aws-glue';
+import { Role, ServicePrincipal, ManagedPolicy } from 'aws-cdk-lib/aws-iam';
 
 export class DataIngestionStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // Create the most basic S3 bucket (example from your original stack)
+
+    // Create a unique S3 bucket name using the stack name and account ID
+    const uniqueBucketName = `gps-dynamodb-glue-data-${this.account}-${this.stackName}`;
+
+    // Create the S3 bucket with the unique name
+    const s3Bucket = new s3.Bucket(this, 'UniqueGPSDataBucket', {
+      bucketName: uniqueBucketName.toLowerCase(),  // Ensure bucket name is lowercase
+      removalPolicy: cdk.RemovalPolicy.DESTROY,        // Bucket will be deleted with the stack
+    });
+
+    // Output the bucket name to the console
+    new cdk.CfnOutput(this, 'BucketNameOutput', {
+      value: s3Bucket.bucketName,
+    });
+
+    // Create the most basic S3 bucket (example from your original stack) this is for testing
+    // TODO: remove this - for testing only
     const bucket = new s3.Bucket(this, 'BasicS3Bucket', {
       removalPolicy: cdk.RemovalPolicy.DESTROY, // Ensures the bucket is deleted when the stack is destroyed
       autoDeleteObjects: true,  // Automatically delete objects when the bucket is deleted
@@ -77,10 +95,42 @@ export class DataIngestionStack extends cdk.Stack {
     // EXPLICIT: Make sure the Lambda function is destroyed
     gpsTopicProcessorLambda.applyRemovalPolicy(cdk.RemovalPolicy.DESTROY);
 
-    // Output the bucket name to the console
-    new cdk.CfnOutput(this, 'BucketNameOutput', {
-      value: bucket.bucketName,
-      description: 'The name of the S3 bucket',
+    // ***************** create glue crawlers for Dynamodb
+    // Step 1: Create IAM Role for AWS Glue
+    const glueRole = new Role(this, 'GlueDynamoDBRole', {
+      assumedBy: new ServicePrincipal('glue.amazonaws.com'),
     });
+
+    // Attach necessary policies
+    glueRole.addManagedPolicy(ManagedPolicy.fromAwsManagedPolicyName('service-role/AWSGlueServiceRole'));
+    glueRole.addManagedPolicy(ManagedPolicy.fromAwsManagedPolicyName('AmazonDynamoDBReadOnlyAccess'));
+    glueRole.addManagedPolicy(ManagedPolicy.fromAwsManagedPolicyName('AmazonS3FullAccess'));
+
+    // Step 2: Create AWS Glue Database (for storing the metadata from the crawler)
+    const glueDatabase = new CfnDatabase(this, 'GPSDataCatalog', {
+      catalogId: this.account,
+      databaseInput: {
+        name: 'gps_data_catalog',  // Database name in Glue Data Catalog
+      },
+    });
+
+    // Step 3: Create Glue Crawler
+    const glueCrawler = new CfnCrawler(this, 'DynamoDBGPSCrawler', {
+      role: glueRole.roleArn,  // Attach the IAM Role to the crawler
+      databaseName: glueDatabase.ref,  // Target Glue Database
+      name: 'DynamoDBgps',
+      targets: {
+        dynamoDbTargets: [
+          {
+            path: 'GpsDataTable',  // DynamoDB Table Name
+          },
+        ],
+      },
+      schedule: {
+        scheduleExpression: 'cron(0 12 * * ? *)',  // Optional: Crawler schedule, adjust if needed
+      },
+      tablePrefix: 'gps_',  // Optional: Prefix for tables created by the Crawler
+    });
+
   }
 }
